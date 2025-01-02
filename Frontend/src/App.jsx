30d6ed92b6fc34eb0d97411f19607d90b742f05d
Rahul <rahuldevan2019@gmail.com>
2025-01-02 18:31:04 +0530

    added homelayout and homepage ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 
 import { Route,Routes } from 'react-router-dom';
-import Footer from './components/Footer.jsx';
+
+import HomePage from './Pages/HomePage.jsx';
 
 function App() {
 
   return (
-    // <Routes>
-    //   <Route path='/' element={<Footer/>}></Route>
-    // </Routes>
-    <Footer/>
+    <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+    </Routes>
   )
 }
 
