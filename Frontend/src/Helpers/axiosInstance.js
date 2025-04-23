11a4255fc6722edde2d5519a7fc73ccca8a4ca65
Rahul <rahuldevan2019@gmail.com>
2025-04-24 01:26:11 +0530

    backend link

--- Frontend/src/Helpers/axiosInstance.js
+++ Frontend/src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL="http://localhost:8000/api/v1";
+const BASE_URL="https://lms-learning-management-system-mntn.vercel.app/api/v1";
 
 const axiosInstance=axios.create();
 
