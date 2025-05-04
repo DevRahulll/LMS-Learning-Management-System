ec1a589e5e075719472ed2405213269ed2157361
Rahul <rahuldevan2019@gmail.com>
2025-05-04 23:32:08 +0530

    changes in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,6 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: [process.env.FRONTEND_URI],
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
 app.use(cookieParser());
